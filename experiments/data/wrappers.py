acbefecf148debd705cb901cd4d959c6d69df2fd
TariqAHassan <TariqAHassan@users.noreply.github.com>
2022-01-22 19:42:19 -0400

    Add docstrings

--- experiments/data/wrappers.py
+++ experiments/data/wrappers.py
@@ -6,7 +6,7 @@
 from functools import partial
 from multiprocessing import cpu_count
 from pathlib import Path
-from typing import Any, Type
+from typing import Any, Optional, Type
 
 import torch
 from torch.utils.data import DataLoader, Dataset, random_split
@@ -33,13 +33,25 @@ def _train_val_split(
 
 
 class DatasetWrapper:
+    """High-level Dataset Wrapper.
+
+    Wrapper to standardize different datasets.
+
+    Args:
+        dataset (Dataset): a dataset with (data) `root` as the first argument
+        val_prop (float): proportion of the data to use for validation
+        seed (int): seed to use when splitting the data into training
+            and validation datasets
+
+    """
+
     def __init__(
         self,
         dataset: Type[Dataset],
         val_prop: float = 0.1,
         seed: int = 42,
     ) -> None:
-        self.dataset = dataset(self.root_dir)   # noqa
+        self.dataset = dataset(self.root_dir)  # noqa
         self.val_prop = val_prop
         self.seed = seed
 
@@ -51,33 +63,56 @@ class DatasetWrapper:
 
     @property
     def name(self) -> str:
+        """Name of the dataset wrapper. (Used in data storage path.)"""
         return self.dataset.__class__.__name__
 
     @property
     def root_dir(self) -> Path:
+        """Directory where data is stored."""
         path = Path("~/datasets").expanduser().joinpath(self.name)
         path.mkdir(parents=True, exist_ok=True)
         return path
 
     @property
     def classes(self) -> list[str]:
+        """Name of each class in the dataset."""
         raise NotImplementedError()
 
     @property
     def n_classes(self) -> int:
+        """Number of classes in the dataset."""
         return len(self.classes)
 
     @property
     def shape(self) -> tuple[int, ...]:
+        """Shape of the data in the dataset."""
         raise NotImplementedError()
 
     def get_dataloaders(
         self,
         batch_size: int,
         num_workers: int = max(1, cpu_count() - 1),
-        pin_memory: bool = True,
+        pin_memory: Optional[bool] = True,
         **kwargs: Any,
     ) -> tuple[DataLoader, Dataset]:
+        """Get training and validation dataloaders.
+
+        Args:
+            batch_size (int): number of samples in each path
+            num_workers (int): number of subprocesses to use for data loading
+            pin_memory (bool, optional): if ``True`` tensors will be copied into
+                CUDA pinned memory prior to being emitted. If ``None`` this will
+                be determined automatically based on the availability of a device
+                with CUDA support (GPU).
+            **kwargs (Keyword Arguments): keyword arguments to pass
+                to ``DataLoader()``
+
+        Returns:
+            dataloaders (tuple): a tuple of the form
+                ``(train_dataloader, validation_dataloader)``
+
+        """
+
         def make_dataloader(dataset: Dataset, shuffle: bool) -> DataLoader:
             return DataLoader(
                 dataset=dataset,
