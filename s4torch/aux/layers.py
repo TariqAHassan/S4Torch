4dc20ea9056f98ec9d21d4dab8dac646d5438c1c
TariqAHassan <TariqAHassan@users.noreply.github.com>
2022-01-23 15:06:36 -0400

    Refactor: inherit from ABC

--- s4torch/aux/layers.py
+++ s4torch/aux/layers.py
@@ -6,18 +6,24 @@
 from __future__ import annotations
 
 import re
+from abc import ABC
 
 import torch
 from torch import nn
 
 
-class TemporalBasePooling:
+class TemporalBasePooling(ABC):
     kernel_size: int | tuple[int]
 
     @property
     def type(self) -> str:
         (type_,) = re.findall(r"Temporal(.*)Pooling", string=self.__class__.__name__)
         return type_.lower()
+
+
+class TemporalAvgPooling(TemporalBasePooling, nn.AvgPool1d):
+    def forward(self, input: torch.Tensor) -> torch.Tensor:  # noqa
+        return super().forward(input.transpose(-2, -1)).transpose(-2, -1)
 
 
 class TemporalMaxPooling(TemporalBasePooling, nn.MaxPool1d):
