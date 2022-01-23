c5f1f020b7494635c90794ecd0243ab215ceea08
TariqAHassan <TariqAHassan@users.noreply.github.com>
2022-01-23 12:39:43 -0400

    Refactor

--- experiments/utils.py
+++ experiments/utils.py
@@ -8,19 +8,6 @@ from pathlib import Path
 import torch
 
 
-def to_sequence(x: torch.Tensor) -> torch.Tensor:
-    if x.ndim <= 1:
-        raise IndexError(f"Input must be at least 2D, got {x.ndim}D")
-    elif x.ndim == 2:
-        return x.unsqueeze(-1)
-    elif x.ndim == 3:
-        return x
-    elif x.ndim == 4:
-        return x.flatten(2).transpose(-2, -1)
-    else:
-        raise IndexError(f"Expected 2D, 3D or 4D data, got {x.ndim}D")
-
-
 class OutputPaths:
     def __init__(self, output_dir: str, run_name: str) -> None:
         self.output_dir = Path(output_dir).expanduser().absolute()
@@ -37,3 +24,16 @@ class OutputPaths:
         path = self.output_dir.joinpath(f"checkpoints/{self.run_name}")
         path.mkdir(parents=True, exist_ok=True)
         return path
+
+
+def to_sequence(x: torch.Tensor) -> torch.Tensor:
+    if x.ndim <= 1:
+        raise IndexError(f"Input must be at least 2D, got {x.ndim}D")
+    elif x.ndim == 2:
+        return x.unsqueeze(-1)
+    elif x.ndim == 3:
+        return x
+    elif x.ndim == 4:
+        return x.flatten(2).transpose(-2, -1)
+    else:
+        raise IndexError(f"Expected 2D, 3D or 4D data, got {x.ndim}D")
