95203ac4da1e31364a8ba462204aae1d9265b0d7
TariqAHassan <TariqAHassan@users.noreply.github.com>
2022-01-23 12:09:28 -0400

    Fix docstring formatting

--- train.py
+++ train.py
@@ -176,8 +176,8 @@ def main(
         n_blocks (int): number of S4 blocks to construct
         s4_n (int): dimensionality of the state representation
         p_dropout (float): probability of elements being set to zero
-        pooling (str, optional): pooling method to use. Options: ``None``, ``"max_KERNEL_SIZE"``,
-            ``"avg_KERNEL_SIZE"``. Example: ``"avg_2"``.
+        pooling (str, optional): pooling method to use. Options: ``None``, 
+            ``"max_KERNEL_SIZE"``, ``"avg_KERNEL_SIZE"``. Example: ``"avg_2"``.
         norm_type (str, optional): type of normalization to use.
             Options: ``batch``, ``layer``, ``None``.
         max_epochs (int, optional): maximum number of epochs to train for
