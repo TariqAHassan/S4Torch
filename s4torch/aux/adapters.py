f2c5aa1a0acf02a9c779b7e32e8742c1c3ab273c
TariqAHassan <TariqAHassan@users.noreply.github.com>
2022-01-24 23:14:37 -0400

    Refactor

--- s4torch/aux/adapters.py
+++ s4torch/aux/adapters.py
@@ -8,12 +8,12 @@ from torch import nn
 
 
 class TemporalAdapter(nn.Module):
-    def __init__(self, engine: nn.Module) -> None:
+    def __init__(self, wrapped: nn.Module) -> None:
         super().__init__()
-        self.add_module("engine", engine)
+        self.add_module("wrapped", wrapped)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:  # [BATCH, SEQ_LEN, D_MODEL]
-        return self.engine(x.transpose(-2, -1)).transpose(-2, -1)
+        return self.wrapped(x.transpose(-2, -1)).transpose(-2, -1)
 
 
 if __name__ == "__main__":
@@ -24,5 +24,5 @@ if __name__ == "__main__":
     EXPECTED_SHAPE = (BATCH_SIZE, SEQ_LEN // KERNEL_SIZE, HIDDEN_DIM)
 
     x = torch.randn(BATCH_SIZE, SEQ_LEN, HIDDEN_DIM)
-    for engine in (nn.AvgPool1d(2), nn.MaxPool1d(2)):
-        assert TemporalAdapter(engine)(x).shape == EXPECTED_SHAPE
+    for wrapped in (nn.AvgPool1d(2), nn.MaxPool1d(2)):
+        assert TemporalAdapter(wrapped)(x).shape == EXPECTED_SHAPE
